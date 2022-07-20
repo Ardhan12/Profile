2e00c6a47c5632aa2136663c76237e787de9b234
Ardhan12 <arief.ramadhan43@gmail.com>
2022-07-20 17:15:30 +0700

    Initial Commit

---
+++ Profile/ProfileApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProfileApp.swift
+//  Profile
+//
+//  Created by Arief Ramadhan on 20/07/22.
+//
+
+import SwiftUI
+
+@main
+struct ProfileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
